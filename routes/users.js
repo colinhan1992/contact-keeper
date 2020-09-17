3b40ff8930e3e0bfac2ecfc4d662632f1e6ec4d0
ColinHan <mr.colinhan@gmail.com>
2020-09-16 21:23:42 -0500

    adding jwt token

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator/check');
+const config = require('config');
 
 const User = require('../models/User');
 
@@ -47,7 +49,23 @@ router.post(
 
       await user.save();
 
-      res.send('User saved');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 3600 * 48
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
