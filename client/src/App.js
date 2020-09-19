1eca6735fb7eeb5d5347cf14bf2e6dcd4d99b5d3
ColinHan <mr.colinhan@gmail.com>
2020-09-19 14:44:03 -0500

    adding alert context, state, and component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/navbar';
+import Navbar from './components/layout/Navbar';
+import Alerts from './components/layout/Alerts';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
@@ -8,27 +9,31 @@ import Login from './components/auth/Login';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
   return (
-    <AuthState>
-      <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home}></Route>
-                <Route exact path="/about" component={About}></Route>
-                <Route exact path="/register" component={Register}></Route>
-                <Route exact path="/login" component={Login}></Route>
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
-      </ContactState>
-    </AuthState>
+    <AlertState>
+      <AuthState>
+        <ContactState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts></Alerts>
+                <Switch>
+                  <Route exact path="/" component={Home}></Route>
+                  <Route exact path="/about" component={About}></Route>
+                  <Route exact path="/register" component={Register}></Route>
+                  <Route exact path="/login" component={Login}></Route>
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </ContactState>
+      </AuthState>
+    </AlertState>
   );
 };
 
