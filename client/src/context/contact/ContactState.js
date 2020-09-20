ae744f312d46eb5fca7656599cc5586b8cb1fd67
ColinHan <mr.colinhan@gmail.com>
2020-09-19 19:27:30 -0500

    adding delete contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -60,8 +60,20 @@ const ContactState = props => {
   };
 
   // Delete Contact
-  const deleteContact = id => {
-    dispatch({ type: DELETE_CONTACT, payload: id });
+  const deleteContact = async id => {
+    try {
+      const res = await axios.delete('/api/contacts/' + id);
+
+      dispatch({
+        type: DELETE_CONTACT,
+        payload: id
+      });
+    } catch (error) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: error.response.msg
+      });
+    }
   };
 
   // Clear Contacts
